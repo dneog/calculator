ed8d43a01e4efa8dd0e3792587cb0d80a4098fb4
unknown <debashisneog22@gmail.com>
2023-03-12 14:00:34 +0530

    Simple Calculator Code

---
+++ index.js
@@ -0,0 +1,12 @@
+let num1=2;
+let num2=2;
+
+let addition= num1+num2;
+let substraction= num1-num2;
+let multiplication= num1*num2;
+let division= num1/num2;
+
+console.log(addition);
+console.log(substraction);
+console.log(multiplication);
+console.log(division);
